69edfe7c40bd92fe17a7af38010391f0b297ad39
sushanta <sushanta.sapkota10@gmail.com>
2025-11-15 18:26:24 -0500

    Added project structure and airport file

---
+++ src/core/aircraft.py
@@ -0,0 +1,8 @@
+class Aircarft:
+    def __init__(self, x, y , heading_degrees , speed):
+        self.x = x
+        self.y = y
+        self.heading_degree = heading_degrees
+        self.speed = speed
+    
+         
