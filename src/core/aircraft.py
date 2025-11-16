ace631ba2e7b6d2b417f5e6ceed3250eaa7d35fa
sushanta <sushanta.sapkota10@gmail.com>
2025-11-15 19:55:30 -0500

    add

--- src/core/aircraft.py
+++ src/core/aircraft.py
@@ -1,12 +1,39 @@
+import math 
+
 class Aircarft:
-    def __init__(self, x: int , y: int , heading_degrees: int , speed :int):
+    def __init__(self, x: int , y: int , heading_degrees: int , speed :int , altitude: int , verticle_speed):
         self.x = x
         self.y = y
         self.heading_degree = heading_degrees
         self.speed = speed
-    
+        self.altitude = altitude
+        self.verticle_speed = 0
+
     def turn(self , delta_degree :int ):
-        self.heading_degree = (self.heading_degree + delta_degree)% 360
+        self.heading_degree = (self.heading_degree + delta_degree)% 360 #updates the currents aircraft heading
+   
     def set_heading(self, traget_degree :int):
-        self.heading_degree = traget_degree % 360
+        self.heading_degree = traget_degree % 360 
     
+    def update(self, last_update_time :float):
+       
+        #calculate movement 
+        rad = math.radians(self.heading_degree)   #convert it into radian(for python trig (it cant use degrees) )
+        dx = math.cos(rad) * self.speed * last_update_time  # cos - hortizontal
+        dy = math.sin(rad) * self.speed * last_update_time  # sin - verticle
+        
+        #update postion of the aircraft 
+        self.x = self.x + dx
+        self.y = self.y + dy
+    
+    def set_altitude(self, target_altitude:int):
+        self.altitude = max(0 , target_altitude)
+
+    #updates the alititube( example - climb 1000 feet, + 1000 to the orginal altitude)
+    def change_altitude(self, delta_altitude:int):
+        new_altitude = self.altitude + delta_altitude
+        max(0, new_altitude)
+
+
+
+
